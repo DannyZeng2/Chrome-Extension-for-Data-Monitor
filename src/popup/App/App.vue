7c7dce8b98309ed5b82d6129f81d8e5ca7db4f12
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2021-03-13 21:01:36 +0800

    update ui

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,39 +1,81 @@
 <template>
   <div id="main">
-    <h2>SPS Monitor Tool</h2>
-
-    <el-tabs v-model="activeName" type="card">
-      <el-tab-pane label="sps" name="first">
+    <!-- SPS Version -->
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="sps" name="sps">
         <div v-for="item in this.env" :key="item.message">
           <div><span class="prs-version">{{ item }} PRS</span><span class="dom-version">{{ item }} DOM</span></div>
           <div>
-            <button class="status"></button>
-            <button class="status"></button>
+            <span class="status">
+              <div class="update-version">24.9.0.2.1557</div>
+              <div class="update-time">2021-03-12 19:09</div>
+            </span>
+            <span class="status">
+              <div class="update-version">24.9.0.2.1557</div>
+              <div class="update-time">2021-03-12 19:09</div>
+            </span>
           </div>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="sppm" name="second">
+
+      <!-- SPPM Version -->
+      <el-tab-pane label="sppm" name="sppm">
        <div v-for="item in this.env" :key="item.message">
          <div><span class="prs-version">{{ item }} PRS</span><span class="dom-version">{{ item }} DOM</span></div>
          <div>
-           <button class="status"></button>
-           <button class="status"></button>
+           <span class="status">
+             <div class="update-version">24.9.0.2.1557</div>
+             <div class="update-time">2021-03-12 19:09</div>
+           </span>
+           <span class="status">
+             <div class="update-version">24.9.0.2.1557</div>
+             <div class="update-time">2021-03-12 19:09</div>
+           </span>
          </div>
        </div>
       </el-tab-pane>
 
-      <el-tab-pane label="spsi" name="third">
+      <!-- SPSI Version -->
+      <el-tab-pane label="spsi" name="spsi">
         <div v-for="item in this.env" :key="item.message">
           <div><span class="prs-version">{{ item }} PRS</span><span class="dom-version">{{ item }} DOM</span></div>
           <div>
-            <button class="status"></button>
-            <button class="status"></button>
+            <span class="status">
+              <div class="update-version">24.9.0.2.1557</div>
+              <div class="update-time">2021-03-12 19:09</div>
+            </span>
+            <span class="status">
+              <div class="update-version">24.9.0.2.1557</div>
+              <div class="update-time">2021-03-12 19:09</div>
+            </span>
           </div>
         </div>
       </el-tab-pane>
+      <!-- Other Tools -->
+      <el-tab-pane label="tools" name="tools" style="padding-left:10px;height: 450px ">
+        <el-button-group>
+          <el-button class="tool-button" type="primary" size="mini">Clear Mask</el-button>
+          <el-button class="tool-button" type="primary" size="mini">Iris Debug</el-button>
+        </el-button-group>
+        <el-button-group>
+          <el-button id="sps-wiki" class="tool-button" type="primary" size="mini">SPS Wiki</el-button>
+          <el-button id="sps-kanban" class="tool-button" type="primary" size="mini">SPS Kanban</el-button>
+        </el-button-group>
+        <el-divider>Open Story</el-divider>
+        <el-row>
+          <el-col :span="15"><el-input  size="mini" v-model="input" placeholder="Please input story No."></el-input></el-col>
+          <el-col :span="9"><el-button class="trigger-btn" type="primary" size="mini">Go</el-button></el-col>
+        </el-row>
 
-      <el-tab-pane label="tools" name="fourth">
-
+        <el-divider>Fly to QA/PP</el-divider>
+        <el-row>
+          <el-col :span="15">
+            <el-select size="mini" v-model="value" clearable placeholder="Please select env">
+            <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          </el-select>
+          </el-col>
+          <el-col :span="9"><el-button class="trigger-btn" type="primary" size="mini">Go</el-button></el-col>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
 
@@ -46,20 +88,40 @@ export default {
     name: 'app',
     data() {
         return {
-            activeName: 'first',
-            env:['QA1','QA2','QA3','QA4','OOCL UAT','COSCO UAT','OOCL PP','COSCO PP','OOCL PROD','COSCO PROD']
+          options: [{
+            value: '选项1',
+              label: 'QA1'
+          }, {
+            value: '选项2',
+            label: 'QA2'
+          }, {
+            value: '选项3',
+            label: 'QA3'
+          }, {
+            value: '选项4',
+            label: 'QA4'
+          }, {
+            value: '选项5',
+            label: 'OOCL PP'
+          }, {
+            value: '选项5',
+            label: 'COSCO PP'
+          }],
+          value: '',
+          activeName: 'sps',
+          env:['QA1','QA2','QA3','QA4','COSCO UAT','OOCL PP','COSCO PP','OOCL PROD','COSCO PROD']
         };
     },
     methods:{
-
     }
 }
 </script>
 
 <style>
   #main {
-    width: 300px;
+    width: 320px;
     padding-bottom: 10px;
+    padding-left: 15px;
   }
   .prs-version{
     margin-left: 15px;
@@ -69,17 +131,37 @@ export default {
     margin: auto;
     position: absolute;
     left: 0px;
-    margin-left: 160px;
+    margin-left: 175px;
     font-weight: bold;
+  }
+  .update-version {
+    margin-top: 2px;
+    margin-left: 20px;
+  }
+  .update-time {
+    margin-top: 3px;
+    margin-left: 20px;
   }
   .status{
     width: 135px;
-    height: 50px;
-    margin-bottom: 2px;
-    margin-left:10px;
-    background-color: #67c23a;
+    height: 40px;
+    margin-right: 25px;
+    background: #80ec80;
+    display:inline-block;
     border-radius:5px;
-    border:none;
-    outline: none;
+    font-size: xx-small;
+  }
+  .status:hover{
+    background-color: #5fc25f;
+  }
+  .tool-button{
+    width: 135px;
+    height: 30px;
+    margin-bottom: 10px;
+  }
+  .trigger-btn{
+    width: 80px;
+    height: 28px;
+    margin-left: 20px ;
   }
 </style>
--- src/popup/index.js
+++ src/popup/index.js
@@ -3,16 +3,26 @@ import AppComponent from "./App/App.vue";
 
 Vue.component("app-component", AppComponent);
 
-import {Button,Tabs,TabPane} from 'element-ui';
+import {Button,Tabs,TabPane,Col,Row,Input,Divider,Select,Option} from 'element-ui';
 
 
 Vue.use(Button);
 Vue.use(Tabs);
 Vue.use(TabPane);
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(Input);
+Vue.use(Divider);
+Vue.use(Select);
+Vue.use(Option);
 
 new Vue({
     el: "#app",
     render: createElement => {
         return createElement(AppComponent);
     }
+});
+
+document.getElementById("sps-wiki").addEventListener("click",function () {
+    chrome.tabs.create({url: 'https://www.baidu.com/'});
 });
